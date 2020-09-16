0e50f8942487790fbfca57f2216ffb9d728088ee
josiah <benjosiah90@gmail.com>
2020-09-15 19:58:31 -1100

    basic

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'App',
+  data(){
+    return
+  },
+     created(){
+        let vm=this
+
+    axios.get('/tasks')
+      .then(response => {
+        vm.todos=response.data
+        // console.log(response.data)
+        vm.todos.forEach(element => {
+            element.updating=false
+        });
+        // console.log(this.todos)
+      })
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0%;
+  padding: 0px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Todo from '@/components/Todos'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'todo',
+      component: Todo
+    }
+  ],
+  mode:'history'
+})
